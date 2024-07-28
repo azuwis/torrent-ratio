2a3a85599b7f09cf1d2d19dd0c15dfc3c516a211
Zhong Jianxin <azuwis@gmail.com>
2024-07-29 00:14:36 +0800

    flake: Format using nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs.nixpkgs.url = "nixpkgs";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       eachSystem = nixpkgs.lib.genAttrs [
         "aarch64-darwin"
@@ -19,61 +20,80 @@
       ];
     in
     {
-      packages = eachSystem (system:
+      packages = eachSystem (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           torrent-ratio = pkgs.buildGoModule {
             name = "torrent-ratio";
-            src = with pkgs.lib.fileset; toSource {
-              root = ./.;
-              fileset = difference
-                ./.
-                (unions [
+            src =
+              with pkgs.lib.fileset;
+              toSource {
+                root = ./.;
+                fileset = difference ./. (unions [
                   (maybeMissing ./result)
                   ./.github
                   ./flake.lock
                   ./flake.nix
                 ]);
-            };
+              };
             vendorHash = "sha256-4NAwh2sp1SBVniMmx6loFMN/9gbY3kfWnHV/U0TIgHg=";
-            ldflags = [ "-s" "-w" ];
+            ldflags = [
+              "-s"
+              "-w"
+            ];
           };
-          mkCrossPackage = gosystem:
+          mkCrossPackage =
+            gosystem:
             let
               GOARCH = builtins.elemAt (builtins.split "_" gosystem) 2;
               GOOS = builtins.elemAt (builtins.split "_" gosystem) 0;
-              ZIGARCH = { arm64 = "aarch64"; amd64 = "x86_64"; }.${GOARCH};
+              ZIGARCH =
+                {
+                  arm64 = "aarch64";
+                  amd64 = "x86_64";
+                }
+                .${GOARCH};
               ZIGOS = { darwin = "macos"; }.${GOOS} or GOOS;
               lib = pkgs.lib;
-              zigExtraArgs = with nixpkgs.legacyPackages.aarch64-darwin.darwin.apple_sdk; lib.optionalString (GOOS == "darwin")
-                " -isystem ${Libsystem}/include -F${frameworks.CoreFoundation}/Library/Frameworks -F${frameworks.Security}/Library/Frameworks";
+              zigExtraArgs =
+                with nixpkgs.legacyPackages.aarch64-darwin.darwin.apple_sdk;
+                lib.optionalString (GOOS == "darwin")
+                  " -isystem ${Libsystem}/include -F${frameworks.CoreFoundation}/Library/Frameworks -F${frameworks.Security}/Library/Frameworks";
             in
-            torrent-ratio.overrideAttrs (old: {
-              inherit GOARCH GOOS;
-              nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.zig ];
-              preBuild = (old.preBuild or "") + ''
-                export XDG_CACHE_HOME="$TMPDIR"
-                export CC="zig cc -target ${ZIGARCH}-${ZIGOS}${zigExtraArgs}"
-                export CXX="$CC"
-              '';
-              postInstall = (old.postInstall or "") + ''
-                if [ -d $out/bin/${gosystem} ]
-                then
-                  mv $out/bin/${gosystem}/* $out/bin
-                  rmdir $out/bin/${gosystem}/
-                fi
-              '';
-            } // lib.optionalAttrs (gosystem == "darwin_amd64") {
-              # https://github.com/ziglang/zig/issues/15438
-              NIX_HARDENING_ENABLE = "pie";
-            });
+            torrent-ratio.overrideAttrs (
+              old:
+              {
+                inherit GOARCH GOOS;
+                nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.zig ];
+                preBuild =
+                  (old.preBuild or "")
+                  + ''
+                    export XDG_CACHE_HOME="$TMPDIR"
+                    export CC="zig cc -target ${ZIGARCH}-${ZIGOS}${zigExtraArgs}"
+                    export CXX="$CC"
+                  '';
+                postInstall =
+                  (old.postInstall or "")
+                  + ''
+                    if [ -d $out/bin/${gosystem} ]
+                    then
+                      mv $out/bin/${gosystem}/* $out/bin
+                      rmdir $out/bin/${gosystem}/
+                    fi
+                  '';
+              }
+              // lib.optionalAttrs (gosystem == "darwin_amd64") {
+                # https://github.com/ziglang/zig/issues/15438
+                NIX_HARDENING_ENABLE = "pie";
+              }
+            );
         in
-        eachGoSystem mkCrossPackage // {
-          default = torrent-ratio;
-        }
+        eachGoSystem mkCrossPackage // { default = torrent-ratio; }
       );
 
-      devShells = eachSystem (system:
+      devShells = eachSystem (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
@@ -84,6 +104,7 @@
               sqlite
             ];
           };
-        });
+        }
+      );
     };
 }
